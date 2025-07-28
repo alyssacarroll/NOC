1528358f99a51a70a826d83e3811d00d596a8ead
alyssacarroll <alyssa.carroll.22@cnu.edu>
2025-07-28 12:51:58 -0400

    reverted URLS

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 (() => {
-  const CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQohLpgMCkMfv1T6kUto-ole2NRCZCzfywnVNl98gxNFSY2BbFN4bVCpZu3QsL3DKLhwNqcqw8C8inf/pub?output=csv";
-  const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwEg3zDQTvzrkZPgnaIMRBT9InrP-Hz_yT7dOCK9KsXcOBPjnqqMjqtAjRG3f_zd3ae/exec";
+  const CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRVOcAqswtmyk_vtsIiOeBy6zwv_jT4pxx_UOBlcsr-n4cxa0PnFluIvTP_E9yXk3nCB-SLWi_cOSiW/pub?output=csv";
+  const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxuQKu1YOgZvkhwhnOTdqAEnUMFCm7SkKziz6iqAU3YF4eyjHKglIONFgYLkMFo6X6-Tw/exec";
 
   // ------- Utilities --------
   function formatDate(date = new Date()) {
@@ -303,17 +303,17 @@
   }
 
   function submitSpreadsheetUrl() {
-  const input = document.getElementById("spreadsheetUrlInput");
-  const url = input.value.trim();
+    const input = document.getElementById("spreadsheetUrlInput");
+    const url = input.value.trim();
 
-  if (!url) {
-    alert("Please enter a URL.");
-    return;
+    if (!url) {
+      alert("Please enter a URL.");
+      return;
+    }
+
+    localStorage.setItem("messageSpreadsheetUrl", url);
+    alert("URL saved!");
   }
-
-  localStorage.setItem("messageSpreadsheetUrl", url);
-  alert("URL saved!");
-}
 
 
 
